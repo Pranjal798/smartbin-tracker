0d77c35d9710a4af529295fcd1a18dab6e3a67c4
Pranjal798 <113894861+Pranjal798@users.noreply.github.com>
2025-04-09 12:58:20 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -33,6 +33,8 @@ def delete_bin(bin_id):
     ref.delete()
     return jsonify({"message": "Bin deleted"}), 200
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
